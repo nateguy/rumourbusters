45233927c56caf8bac3e700fbe6d582e3b5cd662
nateguy <kulio@kulios-MacBook-Air.local>
2014-10-12 02:19:10 +0800

    g

--- app/models/rumour.rb
+++ app/models/rumour.rb
@@ -6,5 +6,5 @@ class Rumour < ActiveRecord::Base
 
 
   has_attached_file :media, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :media, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
