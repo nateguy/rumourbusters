061e04ce973ad4fa4e776d3bdd51c7dd8e5e5591
nateguy <kulio@kulios-MacBook-Air.local>
2014-10-15 02:46:17 +0800

    hey

--- app/models/rumour.rb
+++ app/models/rumour.rb
@@ -11,11 +11,10 @@ class Rumour < ActiveRecord::Base
 
 
 
-  has_attached_file :media, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-    :url  => ":attachment/:id/:style/:filename"
+  has_attached_file :media, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   # validates_attachment :media, :content_type => { :content_type => "image/jpeg" }
   validates_attachment_content_type :media, content_type: [/\Aimage\/.*\Z/, "application/octet-stream"]
-  puts :media
+
 
   # def set_mime_type(file)
   #   if file.present? && file.content_type == 'application/octet-stream'
